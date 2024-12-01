6974a6ca470391e4567c51a6c492fe1e6be05f62
rafitalavi <rafitalavi12345@gmail.com>
2024-12-02 00:15:03 +0600

    apilstVote

--- devin/api/views.py
+++ devin/api/views.py
@@ -1,8 +1,48 @@
 from django.http import JsonResponse
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated , IsAdminUser
+from rest_framework.response import Response
+from .serializers import ProjectSerializer
+from projects.models import Project
 
 
+
+@api_view(['GET'])
 def getRoutes(request):
     routes = [
-            {'GET':'api/projects'}
+            {'GET':'api/projects'},
+            {'GET':'api/projects/id'},
+            {'POST':'api/projects/vote'},
 
-    ]
+            {'POST':'api/users/token'},
+            {'POST':'api/users/token/refresh'},
+
+
+   ]
+    return Response(routes ) #safe we add more items than dictionary 
+
+
+@api_view(['GET'])
+# @permission_classes([IsAuthenticated]) #log in required to see all projects
+def getProjects(request):
+    print('USER:' ,request.user)
+    projects = Project.objects.all()
+    serializer = ProjectSerializer(projects , many = True) #converting it into jsn format
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getProject(request , pk):
+    projects = Project.objects.get(id=pk)
+    serializer = ProjectSerializer(projects , many = False) #converting it into jsn format if i want to show one item
+    return Response(serializer.data)
+
+# Voting
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def projectVote(request,pk):
+    project = project.obj.get(id = pk)
+    user = request.user
+    data = request.data
+    print('DATA:',data)
+    serializer = ProjectSerializer(serializer.data , many= False)
+    return Response(serializer.data)
--- devin/devin/urls.py
+++ devin/devin/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('projects/',include('projects.urls')),
     path('', include('users.urls')),
+    path('api/', include('api.urls')),
     path('reset_password/',auth_views.PasswordResetView.as_view(template_name ='reset_password.html'), name='reset_password' ),
     path('reset_password_done/', auth_views.PasswordResetDoneView.as_view(template_name ='reset_password_done.html') , name="password_reset_done"),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name ='password_reset_confirm.html') , name="password_reset_confirm"),
